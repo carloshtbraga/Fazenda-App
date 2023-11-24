b6dd12833abff6774318fa89158513b5eb634837
carloshtbraga <carloshtbraga@gmail.com>
2023-11-24 20:08:38 -0300

    :lipstick:

--- fazenda/forms.py
+++ fazenda/forms.py
@@ -7,21 +7,42 @@ class ClienteForm(ModelForm):
     class Meta:
         model = Cliente
         fields = ["nome", "empresa", "telefone"]
+        widgets = {
+            "nome": forms.TextInput(attrs={"class": "form-control"}),
+            "empresa": forms.TextInput(attrs={"class": "form-control"}),
+            "telefone": forms.TextInput(attrs={"class": "form-control"}),
+        }
 
 
 class ProdutoForm(ModelForm):
     class Meta:
         model = Produto
         fields = ["nome", "tempo_preparo"]
+        widgets = {
+            "nome": forms.TextInput(attrs={"class": "form-control"}),
+            "tempo_preparo": forms.NumberInput(attrs={"class": "form-control"}),
+        }
 
 
 class PedidoForm(ModelForm):
     class Meta:
         model = Pedido
         exclude = ["produtos", "status"]
+        widgets = {
+            "cliente": forms.Select(attrs={"class": "form-control"}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(PedidoForm, self).__init__(*args, **kwargs)
+        self.fields["cliente"].empty_label = "Selecione o Cliente"
 
 
 class ItemPedidoForm(ModelForm):
     class Meta:
         model = ItemPedido
         fields = ["produto", "quantidade", "preco"]
+        widgets = {
+            "produto": forms.Select(attrs={"class": "form-control"}),
+            "quantidade": forms.NumberInput(attrs={"class": "form-control"}),
+            "preco": forms.NumberInput(attrs={"class": "form-control"}),
+        }
